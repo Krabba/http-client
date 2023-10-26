7b92548ce9ff86dc1fb60fc5de8404e769abf503
Krabba <11424371+Krabba@users.noreply.github.com>
2023-10-26 11:40:33 +0200

    Implement @tanstack/react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import { Root } from '@http-client/root'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './tailwind.css'
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Root />
+    <QueryClientProvider client={queryClient}>
+      <Root />
+    </QueryClientProvider>
   </React.StrictMode>
 )
 
--- src/root.tsx
+++ src/root.tsx
@@ -1,7 +1,24 @@
+import { useQuery } from '@tanstack/react-query'
+
+async function queryFn(): Promise<{ message: string }> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const result = {
+        message: 'From example',
+      }
+
+      resolve(result)
+    }, 1000)
+  })
+}
+
 export const Root = () => {
+  const { data, isLoading } = useQuery({ queryKey: ['example'], queryFn })
+
   return (
     <div className='bg-fuchsia-500 p-4 font-bold'>
-      Tailwind now works properly
+      <span>Tailwind now works properly</span>
+      {isLoading ? <div>Loading...</div> : <div>{data?.message}</div>}
     </div>
   )
 }
